4e135e4cf164ebbdd4cbf587e9d9ef1215d4d13e
Nhanh <trvannhanh@gmail.com>
2024-12-22 08:13:52 +0700

    abc

--- hotelapp/models.py
+++ hotelapp/models.py
@@ -68,7 +68,6 @@ class Phong(db.Model):
     maPhong = db.Column(db.Integer, primary_key=True, autoincrement=True)
     trangThaiPhong = db.Column(db.Integer, db.ForeignKey('TrangThaiPhong.maTrangThai'), nullable=False)
     maLoaiPhong = db.Column(db.Integer, db.ForeignKey('LoaiPhong.maLoaiPhong'), nullable=False)
-
     chiTietDatPhong = relationship('ChiTietDatPhong', backref=backref('phong', lazy=True))
     chiTietThuePhong = relationship('ChiTietThuePhong', backref=backref('phong', lazy=True))
     lichSuTrangThai = relationship('LichSuTrangThaiPhong', backref=backref('phong', lazy=True))
@@ -80,7 +79,6 @@ class PhieuDatPhong(db.Model):
     ngayDatPhong = db.Column(db.Date, nullable=False)
     ngayNhanPhong = db.Column(db.Date, nullable=False)
     ngayTraPhong = db.Column(db.Date, nullable=False)
-
     chiTietDatPhong = relationship('ChiTietDatPhong', backref=backref('phieuDatPhong', lazy=True))
     phieuThuePhong = relationship('PhieuThuePhong', backref=backref('phieuDatPhong', lazy=True))
 
@@ -97,7 +95,6 @@ class PhieuThuePhong(db.Model):
     ngayNhanPhong = db.Column(db.Date, nullable=False)
     ngayTraPhong = db.Column(db.Date, nullable=False)
     maNhanVien = db.Column(db.Integer, db.ForeignKey('NhanVien.maNhanVien'), nullable=False)
-
     chiTietThuePhong = relationship('ChiTietThuePhong', backref=backref('phieuThuePhong', lazy=True))
     hoaDon = relationship('HoaDon', backref=backref('phieuThuePhong', lazy=True))
 
